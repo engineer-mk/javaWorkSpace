068836e660112fa1e50361af0ca5424045eb025b
makui <kuim123@gmail.com>
2023-03-28 14:32:59 +0800

    k8s-start

---
+++ k8s-start/push.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC2046
+# shellcheck disable=SC2034
+IMAGE_NAME="k8s-start"
+IMAGE_VERSION="latest"
+REMOTE_ADDRESS="registry-vpc.cn-hangzhou.aliyuncs.com/xmg-docker"
+docker rmi $(docker images -aq -f "dangling=true")
+mvn clean package
+docker build -t ${IMAGE_NAME}:${IMAGE_VERSION} -f src/main/docker/Dockerfile .
+docker tag ${IMAGE_NAME}:${IMAGE_VERSION} ${REMOTE_ADDRESS}/${IMAGE_NAME}:${IMAGE_VERSION}
+docker push ${REMOTE_ADDRESS}/${IMAGE_NAME}:${IMAGE_VERSION}
+
+#scp deployment.yml ubuntu:/home/mk/k8s-yml/k8s-start
