93b015ac81980b263e8997d08a0e8848c00d82a2
makui <kuimgg123@gmail.com>
2023-10-04 15:52:37 +0800

    .

--- springSecurity-demo/src/main/java/com/xmg/controller/AccountController.java
+++ springSecurity-demo/src/main/java/com/xmg/controller/AccountController.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.RequestMapping;
